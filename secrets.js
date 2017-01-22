14df2301837ef17820a84822a96200ff51353247
Julie Busch <heyjuliebusch@gmail.com>
2017-01-22 18:45:50 -0500

    pulling from master after merging email branch

---
+++ secrets.js
@@ -0,0 +1,7 @@
+module.exports = {
+  gmailPw: 'PleaseD0ntHackMe',
+  gmailUser: 'tinytravelbooks@gmail.com',
+  googleApiKey: 'AIzaSyD0qy5_PQelp52UhU9luOqiFDMwC5iF4Jo',
+  googleMapsSecret: 'Q4lIv_0coWtKYmKC7F91eeINpbM='
+
+}
